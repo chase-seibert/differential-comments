ea987c20480ecf4f06b5ffd6306d07bec892ce84
Chase Seibert <cseibert@dropbox.com>
2019-06-06 17:08:42 -0700

    --days works to filter the comments, too

--- lib_phab.py
+++ lib_phab.py
@@ -35,6 +35,11 @@ def get_author_phids(emails):
     return [d['phid'] for d in data]
 
 
+def get_epoc_days_ago(days):
+    after = datetime.now() - timedelta(days=int(days))
+    return int(time.mktime(after.timetuple()))
+
+
 def get_diffs(author_phids, days=None):
     phab = Phabricator()
     constraints = {
@@ -43,8 +48,7 @@ def get_diffs(author_phids, days=None):
     if settings.DIFF_STATUS:
         constraints['statuses'] = settings.DIFF_STATUS
     if days:
-        after = datetime.now() - timedelta(days=int(days))
-        constraints['createdStart'] = int(time.mktime(after.timetuple()))
+        constraints['createdStart'] = get_epoc_days_ago(days)
     results = phab.differential.revision.search(
         constraints=constraints,
         #order='newest',
@@ -52,13 +56,16 @@ def get_diffs(author_phids, days=None):
     return results.get('data')
 
 
-def get_comments(phid, author_phids, exclude_author_phid):
+def get_comments(phid, author_phids, exclude_author_phid, days=None):
     phab = Phabricator()
+    constraints = {
+        'authorPHIDs': author_phids,
+    }
+    if days:
+        constraints['createdStart'] = get_epoc_days_ago(days)
     transactions = phab.transaction.search(
         objectIdentifier='D%s' % phid,
-        constraints={
-            'authorPHIDs': author_phids,
-        }
+        constraints=constraints,
     )
     comments = []
     for transaction in transactions.get('data'):
