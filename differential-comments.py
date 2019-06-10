ded2c5675864aea0084f9e2c64745330490ee1ef
Chase Seibert <cseibert@dropbox.com>
2019-06-10 11:37:31 -0700

    Add single user mode

--- differential-comments.py
+++ differential-comments.py
@@ -15,16 +15,19 @@ def kwargs_or_default(setting_value):
 
 
 def list(args):
-    author_phids = lib_phab.get_author_phids(settings.TEAM_EMAILS)
+    team_phids = lib_phab.get_author_phids(settings.TEAM_EMAILS)
+    commentor_phids = team_phids
+    if args.just_email:
+        commentor_phids = lib_phab.get_author_phids([args.just_email, ])
     counts = Counter()
-    for diff in lib_phab.get_diffs(author_phids, args.days):
+    for diff in lib_phab.get_diffs(team_phids, args.days):
         # import ipdb; ipdb.set_trace()
         diff_phid = diff.get('id')
         fields = diff.get('fields')
         diff_author_phid = fields.get('authorPHID')
         diff_author = lib_phab.phid_to_name(diff_author_phid)
         #print '%s (%s) "%s"' % (diff_phid, author, fields.get('title'))
-        comments = lib_phab.get_comments(diff_phid, author_phids, diff_author_phid)
+        comments = lib_phab.get_comments(diff_phid, commentor_phids, diff_author_phid)
         if not comments:
             continue
         if not args.just_tally:
@@ -46,7 +49,9 @@ def list(args):
                 print '.',
     if args.just_tally:
         print ''
-    print counts
+    print '=== Totals ==='
+    for key, value in counts.items():
+        print '%s: %s' % (key, value)
 
 
 if __name__ == '__main__':
@@ -57,6 +62,7 @@ if __name__ == '__main__':
         **kwargs_or_default(settings.TEAM_EMAILS))
     parser.add_argument('--days', help='How many days back to go')
     parser.add_argument('--just-tally', help='Just print the final tally', action='store_true')
+    parser.add_argument('--just-email', help='Just one user by email address')
     args = parser.parse_args()
     list(args)
     cache.update()
